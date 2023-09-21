30cef9745fc49256aef1646d4fd976aa78bb0261
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-09-20 21:34:09 -0500

    added varibles for all character options, added alert for choosing characters, added variables for valid character options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function generatePassword(){
+  // Write password to the #password input
+  function generatePassword(){
+  // added the alert to the beginning of the password so users would know requirements immediately
+    alert("Safe passwords include:: 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character")
   var length = parseInt(prompt("Enter password using 8 to 128 characters"));
-if (length < 8 || length > 128) {
-  alert("Invalid length");
+
+    if (length < 8 || length > 128) {
+    alert("Invalid length");
+    return;
+    }
+
+  // included variables for all character requirements
+  var useLowercase = confirm("Would you like to include lowercase characters?");
+  var useUppercase = confirm("Would you like to include uppercase characters?");
+  var useNumber = confirm("Would you like to include numberic characters?");
+  var useSpecial = confirm("Would you like to include special characters?");
+
+    if (!useLowercase || !useUppercase || !useNumber || !useSpecial) {
+      alert("Please choose character types");
+      return;
+    }
+
+  // set valid character options
+  var lowercaseCharacter = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberCharacter = "0123456789";
+  var specialCharacter = "!@#$%^&*()_-+=<>?";
+
+  var allCharacters = lowercaseCharacter = uppercaseCharacter = numberCharacter = specialCharacter;
+
 }
-}
+
+
+
+// var characters = confirm("Would you like to include lowercase?")
+
+// include lowercase, uppercase, numeric, and/or special characters
 
 function writePassword() {
   var password = generatePassword();
@@ -19,3 +49,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
